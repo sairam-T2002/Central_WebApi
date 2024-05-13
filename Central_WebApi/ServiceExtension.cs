a443badbb07680bc16612af30101d7214e8df9e3
Sairam <sairam.qwq@gmail.com>
2024-05-13 11:21:19 +0530

    Multiple domains cors enabled

--- Central_WebApi/ServiceExtension.cs
+++ Central_WebApi/ServiceExtension.cs
@@ -7,14 +7,14 @@ namespace Central_WebApi
         public static void AddWebApiCors( this IServiceCollection services, IConfiguration configuration, string policyName )
         {
             var settingsSection = configuration.GetSection("CorsIPs");
-            var settings = settingsSection.Get<string[]>();
+            string[] settings = settingsSection.Get<string[]>();
             settings = settings ?? new string[0];
+
             services.AddCors(options =>
             {
                 options.AddPolicy(policyName, builder =>
                 {
-                    builder.WithOrigins("http://localhost:3000")
-                    //builder.WithOrigins(settings)
+                    builder.WithOrigins(settings)
                         .AllowAnyMethod()
                         .AllowAnyHeader();
                 });
