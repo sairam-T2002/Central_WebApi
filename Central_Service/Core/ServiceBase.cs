e1c2fc600ccdffba2c31b770456cc6103fa6de3e
Sairam <sairam.qwq@gmail.com>
2024-09-11 20:33:54 +0530

    changes

--- Central_Service/Core/ServiceBase.cs
+++ Central_Service/Core/ServiceBase.cs
@@ -12,26 +12,31 @@ namespace Central_Service.Core
     public abstract class ServiceBase
     {
         protected readonly ILogger<ServiceBase> Logger;
-        protected readonly IServiceProvider serviceProvider;
+        protected readonly IServiceProvider _serviceProvider;
         protected ServiceBase( ILogger<ServiceBase> logger, IServiceProvider provider ) {
             Logger = logger;
-            serviceProvider = provider;
+            _serviceProvider = provider;
         }
 
         protected T GetService<T>() where T : notnull
         {
-            return serviceProvider.GetRequiredService<T>();
+            return _serviceProvider.GetRequiredService<T>();
         }
 
         protected object GetService( Type serviceType )
         {
-            object? service = serviceProvider.GetService(serviceType);
+            object? service = _serviceProvider.GetService(serviceType);
             if (service == null)
             {
                 throw new InvalidOperationException("No service for type 'serviceType' has been registered.");
             }
 
             return service;
+        }
+
+        protected IServiceScope CreateScope()
+        {
+            return _serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
         }
     }
 }
--- ExtensionMethods/Extensions.cs
+++ ExtensionMethods/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace ExtensionMethods
 {
@@ -36,5 +38,62 @@ namespace ExtensionMethods
             return temp;
         }
 
+        public static string CapitalizeFirstLetter( this string str )
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            return $"{char.ToUpper(str[0], System.Globalization.CultureInfo.CurrentCulture)}{str.Substring(1)}";
+        }
+
+        public static string ToTitleCase( this string str )
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+            return textInfo.ToTitleCase(str.ToLower());
+        }
+
+        public static string ToCamelCase( this string str )
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+            string titleCase = textInfo.ToTitleCase(str.ToLower());
+            string camelCase = Regex.Replace(titleCase, @"\s+", "");
+
+            if (camelCase.Length > 0)
+            {
+                camelCase = char.ToLower(camelCase[0]) + camelCase.Substring(1);
+            }
+
+            return camelCase;
+        }
+
+        public static bool IsEven(this int value )
+        {
+            return value % 2 == 0;
+        }
+
+        public static bool IsPrime(this int value)
+        {
+            if (value < 2)
+                return false;
+            if(value == 2)
+                return true;
+            if (value.IsEven())
+                return false;
+
+            int sqrt = (int)Math.Sqrt(value);
+            for (int i = 3; i <= sqrt; i += 2)
+            {
+                if (value % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
